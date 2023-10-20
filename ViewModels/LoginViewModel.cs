2a71e2b590ad9b431a303395b987007f54a44267
Shokhrukhbek <Shokhrukhbek@SHKA>
2023-10-20 18:01:41 +0800

    Login View and 50% method

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Tmds.DBus.Protocol;
 
 namespace School_Project.ViewModels;
 
 public class LoginViewModel: ViewModelBase
 {
-    public void Go()
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public string Message { get; set; }
+    public async Task <bool> TryLoginAsync()
     {
-
-    } // write command for login
+        if (Login =="admin" && Password=="admin")
+        {
+            Message = "Access Granted";
+            return true;
+        }
+        return false;
+    } //contruction + commander +reactive
 }
