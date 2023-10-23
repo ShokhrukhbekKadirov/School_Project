55747b54726e1cb736342a2fab5b004cf2937b16
Shokhrukhbek <Shokhrukhbek@SHKA>
2023-10-23 20:21:55 +0800

    New

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using ReactiveUI;
 using Tmds.DBus.Protocol;
 
 namespace School_Project.ViewModels;
@@ -13,7 +15,18 @@ public class LoginViewModel: ViewModelBase
 {
     public string Login { get; set; }
     public string Password { get; set; }
-    public string Message { get; set; }
+    public string Message { 
+        get => _message; 
+        set =>this.RaiseAndSetIfChanged(ref _message, value); }
+
+    private string _message;
+    public LoginViewModel()
+    {
+        LoginCommand = ReactiveCommand.CreateFromTask(TryLoginAsync);
+    }
+
+    public ICommand LoginCommand { get; }
+
     public async Task <bool> TryLoginAsync()
     {
         if (Login =="admin" && Password=="admin")
